ca9c72aafbd45d9d50fe48297bde0f048ad4022e
gR3yF0x78 <simonfoxparker@protonmail.com>
2022-04-16 23:09:30 +0100

    Code update with comments.

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ Model / Data / Main / SP 15/04/2022
 question_bank = []
 
 for question in question_data:
-
+    # The short way
     question_bank.append(Question(question["text"], question["answer"]))
+    # The long way
+    # question_text = question["text"]
+    # question_answer = question["answer"]
+    # new_question = Question(question_text, question_answer)
+    # question_bank.append(new_question)
 
 for question in question_bank:
     print(question.answer)
